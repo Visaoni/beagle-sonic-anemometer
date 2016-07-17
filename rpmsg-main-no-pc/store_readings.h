6984d7fb278f570f468c16aec31a00739ec14f3b
Visaoni <visaoni@gmail.com>
2016-07-17 04:40:44 +0000

    seeing if pin_control is the prob

---
+++ rpmsg-main-no-pc/store_readings.h
@@ -0,0 +1,21 @@
+/*
+ * store_readings.h
+ *
+ * Description:
+ *
+ * (C) 2016 Visaoni
+ * Licensed under the MIT License.
+ */
+
+#ifndef STORE_READINGS_H_
+#define STORE_READINGS_H_
+
+#include <stdint.h>
+
+#define SR_MAX_BUFFER_SIZE 10
+
+void SR_init();
+uint32_t SR_store( uint32_t value );
+void SR_transfer_readings();
+
+#endif /* STORE_READINGS_H_ */
